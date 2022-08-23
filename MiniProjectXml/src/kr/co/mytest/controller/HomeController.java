7559e4946bb0d73e61861666396c370f94fe3a74
zelldingct <zell9931@gmail.com>
2022-08-23 17:07:33 +0900

    220823DoneWithLec42

--- MiniProjectXml/src/kr/co/mytest/controller/HomeController.java
+++ MiniProjectXml/src/kr/co/mytest/controller/HomeController.java
@@ -9,7 +9,7 @@ public class HomeController {
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home() {
-		return "redirect:/main";
+		return "redirect:board/main";
 	}
 	
 	
